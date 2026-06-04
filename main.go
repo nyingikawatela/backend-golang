76e5a7303417b105c58c9902093aa1404e80c192
Nyingika Chimbelengue <nyingikachimbelengue@gmail.com>
2026-06-04 13:36:29 +0100

    addition of validation against profane

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
+	
 
 	_ "golang.org/x/tools/go/cfg"
 )
@@ -21,7 +23,7 @@ type apiConfig struct {
 		Err string `json:"err"`
 	}
 	type resp struct{
-		Valid bool `json:"valid"`
+		Cleaned_body string `json:"cleaned_body"`
 	}
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -65,6 +67,26 @@ func erronAux(msg string, w http.ResponseWriter){
 	w.Write(dat)
 }
 
+func cleanOutput(s string) string{
+	div := strings.Split(s, " ")
+	
+	for i, t := range div{
+		w := strings.ToLower(t)
+		if w == "kerfuffle"{
+			div[i] = strings.ReplaceAll(w, "kerfuffle", "****")
+		}
+		if w == "sharbert"{
+			div[i] = strings.ReplaceAll(w, "sharbert", "****")
+		}
+		if w == "fornax"{
+			div[i] = strings.ReplaceAll(w, "fornax", "****")
+		}
+		
+	}
+	res := strings.Join(div, " ")
+	return res
+}
+
 func validate_chirpHandle(w http.ResponseWriter, r *http.Request){
 	var data parameter;
 	w.Header().Set("Content-Type", "application/json")
@@ -78,8 +100,10 @@ func validate_chirpHandle(w http.ResponseWriter, r *http.Request){
 		erronAux("Chirp is too long", w)
 		return 
 	}
+	retorno := cleanOutput(data.Body)
+	fmt.Println(retorno)
 	res := resp{
-		Valid: true,
+		Cleaned_body: retorno,
 	}
 	dat, _ := json.Marshal(res)
 	w.WriteHeader(http.StatusOK)
